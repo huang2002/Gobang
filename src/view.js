00fc11b1e2ab4156de18fe5d93570eea2fdb36f7
hhh <1340545944@qq.com>
2020-08-16 19:08:17 +0800

    adjust blank style

--- src/view.js
+++ src/view.js
@@ -8,7 +8,7 @@ const RESIZE_DELAY = 100;
 const FOCUS_STROKE = '#000';
 const LAST_MOVE_STROKE = '#36F';
 
-const BLANK_SHADOW = 'inset 2px 2px 2px rgba(0,0,0,.3)';
+const BLANK_SHADOW = 'inset 1px 1px 2px rgba(0,0,0,.3)';
 
 const CHESS_REFLECTION = [ // css box-shadow
     'inset 2px 2px 2px rgba(255,255,255,.3)',
@@ -17,13 +17,13 @@ const CHESS_REFLECTION = [ // css box-shadow
 
 const FILL_STYLES = new Map([
     [BLACK, '#222'],
-    [BLANK, '#EEE'],
+    [BLANK, '#DDD'],
     [WHITE, '#F6F6F6'],
 ]);
 
 const STROKE_STYLES = new Map([
     [BLACK, '#111'],
-    [BLANK, '#888'],
+    [BLANK, '#999'],
     [WHITE, '#333'],
 ]);
 
